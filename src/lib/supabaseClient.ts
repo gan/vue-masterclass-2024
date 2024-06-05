4dcd2f1e012c0fc0328bf8760b4b17fe8f9f4e1c
xgan <xgan2018@163.com>
2024-06-05 10:11:24 +0800

    add supabase

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,11 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+)
+
+
+console.log(import.meta.env.VITE_SUPABASE_URL)
+export default supabase
